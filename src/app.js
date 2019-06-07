24dccd1dec214ab7147ad58d8f91a5cf78b138de
Sasha Marrocco <me@sasha.dev>
2019-06-07 19:24:13 +0100

    Wrap app in a module.exports

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
-class App {
+module.exports = class App {
     static async main() {
         // eslint-disable-next-line no-console
         console.log('Hello world!');
     }
-}
-
-module.exports = App;
+};
